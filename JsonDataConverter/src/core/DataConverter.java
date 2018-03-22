bc95ad4e18a75eb2b6af1701220d6617cd006529
Raphael Fakhir <raphael.fakhir@stud.fh-campuswien.ac.at>
2018-03-22 09:13:18 +0100

    Aktueller Code

--- JsonDataConverter/src/core/DataConverter.java
+++ JsonDataConverter/src/core/DataConverter.java
@@ -39,130 +39,164 @@ public class DataConverter {
 	
 	private static final String FILENAME_JSON_FORMATED = "C://Users/Raphael/git/JsonDataConverter/newDataFormated.json";
 	private static final int MAX_RBL = 8500;
-	private static final String JSON_FILE = "C://Users/Raphael/git/JsonDataConverter/newDataGet.json";
+	private static final String JSON_FILE = "C://Users/Raphael/Desktop/newDataFormated.json";
 	private String REQUEST_URL_All = "http://www.wienerlinien.at/ogd_realtime/monitor?%s&sender=Aq5inVKiQsJwRm9c";
 	
 	private static Logger logger = Logger.getLogger(DataConverter.class); 
 	
 	public static void main(String[] args) throws IOException {
-		PropertyConfigurator.configureAndWatch("DataConverter.log4j.properties");
-		logger.info("Start job");
-		for(int j=0;j<60;j++) {
-			Writer jsonOutput = new BufferedWriter(new FileWriter(JSON_FILE));  //clears file every time
-			DataConverter get_data = new DataConverter();
-			List<String> JSONmonitorlist = get_data.runAll(0, MAX_RBL);
-
-			for (int i = 0; i < JSONmonitorlist.size(); i++) {
-				jsonOutput.append(JSONmonitorlist.get(i) + "\n");
-			}
-			jsonOutput.close();
-
-			int counterSuccess = 0;
-			int counterFailed = 0;
-			BufferedReader input = null;
-			try {
-				input = new BufferedReader( new FileReader( JSON_FILE ));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-
-			Writer output = new BufferedWriter(new FileWriter(FILENAME_JSON_FORMATED));  //clears file every time
-			Date timeR = null;
-			Date timeP = null;
-			String inputData;
-			while (( inputData = input.readLine() ) != null ) {
-				try {
-					JSONObject oldJsonObject = new JSONObject( inputData );
-					logger.info(inputData);
-
-					//				JSONObject _id =oldJsonObject.getJSONObject("_id");
-					//				String id = (String) _id.get("$oid");
-
-					JSONArray lines = (JSONArray) oldJsonObject.get("lines");
-					JSONObject linesObject = lines.getJSONObject(0);
-					String name = (String) linesObject.get("name");
-					String richtungsId = (String) linesObject.get("richtungsId");
-
-					JSONObject departures = linesObject.getJSONObject("departures");
-					JSONArray departure = (JSONArray) departures.get("departure");
-					JSONObject departureObject = departure.getJSONObject(0);
-					JSONObject departureTime = departureObject.getJSONObject("departureTime");
-					String timePlanned = (String) departureTime.get("timePlanned");
-					String timeReal = (String) departureTime.get("timeReal");
-					String serverTime = (String) oldJsonObject.get("serverTime");
-					DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss.SSS");
-					try {
-						timeP = dateFormat.parse(timePlanned);
-						timeR = dateFormat.parse(timeReal);
-					} catch (ParseException e) {
-						logger.warn(e.getMessage());
-					}
-					int delay = (int) ((timeR.getTime()-timeP.getTime())/1000);
-
-					JSONObject locationStop = oldJsonObject.getJSONObject("locationStop");
-					JSONObject properties = locationStop.getJSONObject("properties");
-					String title = (String) properties.get("title");
-
-					JSONObject geometry = locationStop.getJSONObject("geometry");
-					JSONArray location = (JSONArray) geometry.get("coordinates");
-
-					JSONObject attributes = properties.getJSONObject("attributes");
-					Integer rbl = (Integer) attributes.get("rbl");
-
-					String indexJsonString = "{ \"index\" : { \"_index\" : \"wiener_linien\", \"_type\" : \"departures\" } }";
-					String newJsonString = "{\"name\":\""+ name +"\",\"title\":\""+ title +"\",\"rbl\":"+ rbl +",\"richtungsId\":" + richtungsId +",\"delay\":"+ delay +",\"serverTime\":\""+ serverTime +"\",\"location\":"+ location +"}";
-
-					JSONObject indexJsonObject =new JSONObject(indexJsonString);
-					JSONObject newJsonObject = new JSONObject(newJsonString);	
-
-					output.append(indexJsonObject+ "\n");
-					output.append(newJsonObject+ "\n");
-
-					counterSuccess++;
-				} catch (JSONException e) {
-					logger.warn(e.getMessage());
-					counterFailed++;
-				}
-			}
-			output.close();
-			logger.info(counterSuccess + " have successfully been processed" +"\n"+ counterFailed + " have had an error");
-
-			logger.info("Start process Script");
-			try {
-				final Process p = Runtime.getRuntime().exec("script.bat");
-				InputStream os = p.getInputStream();
-				InputStreamReader isr = new InputStreamReader(os);
-
-				int data = isr.read();
-				String info ="";
-				int i = 0;
-				while(data != -1){
-					final char theChar = (char) data;
-					info += theChar;
-					data = isr.read();
-					if (i % 1000 == 0) {
-						logger.info(info);
-						info = "";	
-					}
-					i++;
-				}
-				logger.info(info);
-				isr.close();
-				isr = null;
-				os.close();
-				os = null;
-			} catch (final IOException e) {
-				logger.warn(e.getMessage());
-			}
-			logger.info("End process Script");
-
-			try {
-				TimeUnit.SECONDS.sleep(5);
-			} catch (InterruptedException e) {
-				logger.warn(e.getMessage());
-		    }
+		
+		
+		BufferedReader input = null;
+		try {
+			input = new BufferedReader( new FileReader( JSON_FILE ));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
-	}
+		String inputData;
+		int fileCounter = 0;
+		int lineCounter = 0;
+		Writer output = new BufferedWriter(new FileWriter("C://Users/Raphael/Desktop/splitData/splitData_"+fileCounter+".json"));
+		while (( inputData = input.readLine() ) != null) {
+			lineCounter++;
+			if (lineCounter % 800001 == 0) {
+				output.close();
+				fileCounter++;
+				output = new BufferedWriter(new FileWriter("C://Users/Raphael/Desktop/splitData/splitData_"+fileCounter+".json"));
+				output.append(inputData+ "\n");
+			}else {
+				output.append(inputData+ "\n");
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+//		PropertyConfigurator.configureAndWatch("DataConverter.log4j.properties");
+//		logger.info("Start job");
+		
+//		for(int j=0;j<60;j++) {
+//			Writer jsonOutput = new BufferedWriter(new FileWriter(JSON_FILE));  //clears file every time
+//			DataConverter get_data = new DataConverter();
+//			List<String> JSONmonitorlist = get_data.runAll(0, MAX_RBL);
+//
+//			for (int i = 0; i < JSONmonitorlist.size(); i++) {
+//				jsonOutput.append(JSONmonitorlist.get(i) + "\n");
+//			}
+//			jsonOutput.close();
+
+		
+		
+//			int counterSuccess = 0;
+//			int counterFailed = 0;
+//			BufferedReader input = null;
+//			try {
+//				input = new BufferedReader( new FileReader( JSON_FILE ));
+//			} catch (FileNotFoundException e) {
+//				e.printStackTrace();
+//			}
+//
+//			Writer output = new BufferedWriter(new FileWriter(FILENAME_JSON_FORMATED));  //clears file every time
+//			Date timeR = null;
+//			Date timeP = null;
+//			String inputData;
+//			while (( inputData = input.readLine() ) != null ) {
+//				try {
+//					JSONObject oldJsonObject = new JSONObject( inputData );
+//					logger.info(inputData);
+//
+//					//				JSONObject _id =oldJsonObject.getJSONObject("_id");
+//					//				String id = (String) _id.get("$oid");
+//
+//					JSONArray lines = (JSONArray) oldJsonObject.get("lines");
+//					JSONObject linesObject = lines.getJSONObject(0);
+//					String name = (String) linesObject.get("name");
+//					String richtungsId = (String) linesObject.get("richtungsId");
+//
+//					JSONObject departures = linesObject.getJSONObject("departures");
+//					JSONArray departure = (JSONArray) departures.get("departure");
+//					JSONObject departureObject = departure.getJSONObject(0);
+//					JSONObject departureTime = departureObject.getJSONObject("departureTime");
+//					String timePlanned = (String) departureTime.get("timePlanned");
+//					String timeReal = (String) departureTime.get("timeReal");
+//					String serverTime = (String) oldJsonObject.get("serverTime");
+//					DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'hh:mm:ss.SSS");
+//					try {
+//						timeP = dateFormat.parse(timePlanned);
+//						timeR = dateFormat.parse(timeReal);
+//					} catch (ParseException e) {
+//						logger.warn(e.getMessage());
+//					}
+//					int delay = (int) ((timeR.getTime()-timeP.getTime())/1000);
+//
+//					JSONObject locationStop = oldJsonObject.getJSONObject("locationStop");
+//					JSONObject properties = locationStop.getJSONObject("properties");
+//					String title = (String) properties.get("title");
+//
+//					JSONObject geometry = locationStop.getJSONObject("geometry");
+//					JSONArray location = (JSONArray) geometry.get("coordinates");
+//
+//					JSONObject attributes = properties.getJSONObject("attributes");
+//					Integer rbl = (Integer) attributes.get("rbl");
+//
+//					String indexJsonString = "{ \"index\" : { \"_index\" : \"wiener_linien\", \"_type\" : \"departures\" } }";
+//					String newJsonString = "{\"name\":\""+ name +"\",\"title\":\""+ title +"\",\"rbl\":"+ rbl +",\"richtungsId\":" + richtungsId +",\"delay\":"+ delay +",\"serverTime\":\""+ serverTime +"\",\"location\":"+ location +"}";
+//
+//					JSONObject indexJsonObject =new JSONObject(indexJsonString);
+//					JSONObject newJsonObject = new JSONObject(newJsonString);	
+//
+//					output.append(indexJsonObject+ "\n");
+//					output.append(newJsonObject+ "\n");
+//
+//					counterSuccess++;
+//				} catch (JSONException e) {
+//					logger.warn(e.getMessage());
+//					counterFailed++;
+//				}
+//			}
+//			output.close();
+//			logger.info(counterSuccess + " have successfully been processed" +"\n"+ counterFailed + " have had an error");
+
+//			logger.info("Start process Script");
+//			try {
+//				final Process p = Runtime.getRuntime().exec("script.bat");
+//				InputStream os = p.getInputStream();
+//				InputStreamReader isr = new InputStreamReader(os);
+//
+//				int data = isr.read();
+//				String info ="";
+//				int i = 0;
+//				while(data != -1){
+//					final char theChar = (char) data;
+//					info += theChar;
+//					data = isr.read();
+//					if (i % 1000 == 0) {
+//						logger.info(info);
+//						info = "";	
+//					}
+//					i++;
+//				}
+//				logger.info(info);
+//				isr.close();
+//				isr = null;
+//				os.close();
+//				os = null;
+//			} catch (final IOException e) {
+//				logger.warn(e.getMessage());
+//			}
+//			logger.info("End process Script");
+//
+//			try {
+//				TimeUnit.SECONDS.sleep(5);
+//			} catch (InterruptedException e) {
+//				logger.warn(e.getMessage());
+//		    }
+		}
+//	}
 
 
 	@SuppressWarnings("deprecation")
